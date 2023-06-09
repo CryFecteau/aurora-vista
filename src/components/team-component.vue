44d324bb9bbae000cc2c4dca850a572b6cf26386
Cry <cry.fecteau@gmail.com>
2023-06-08 18:52:49 -0700

    Styled team component

--- src/components/team-component.vue
+++ src/components/team-component.vue
@@ -1,5 +1,109 @@
 <template>
-  <div>team component</div>
+  <div class="container-xxl py-5">
+    <div class="container">
+      <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+        <h1 class="mb-5">Our Outstanding <span class="text-info">Team</span></h1>
+      </div>
+      <div class="row g-4">
+        <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+          <div class="rounded shadow overflow-hidden">
+            <div class="position-relative">
+              <img class="img-fluid" src="https://placehold.co/500" alt="" />
+              <div
+                class="position-absolute start-50 top-100 translate-middle d-flex align-items-center"
+              >
+                <a class="btn btn-square btn-secondary mx-1" href=""
+                  ><i class="bi bi-facebook"></i
+                ></a>
+                <a class="btn btn-square btn-secondary mx-1" href=""
+                  ><i class="bi bi-twitter"></i
+                ></a>
+                <a class="btn btn-square btn-secondary mx-1" href=""
+                  ><i class="bi bi-instagram"></i
+                ></a>
+              </div>
+            </div>
+            <div class="text-center p-4 mt-3">
+              <h5 class="fw-bold mb-0"><span class="text-info">Jace</span> Hendricks</h5>
+              <small>CEO of Events and Adventure</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+          <div class="rounded shadow overflow-hidden">
+            <div class="position-relative">
+              <img class="img-fluid" src="https://placehold.co/500" alt="" />
+              <div
+                class="position-absolute start-50 top-100 translate-middle d-flex align-items-center"
+              >
+                <a class="btn btn-square btn-secondary mx-1" href=""
+                  ><i class="bi bi-facebook"></i
+                ></a>
+                <a class="btn btn-square btn-secondary mx-1" href=""
+                  ><i class="bi bi-twitter"></i
+                ></a>
+                <a class="btn btn-square btn-secondary mx-1" href=""
+                  ><i class="bi bi-instagram"></i
+                ></a>
+              </div>
+            </div>
+            <div class="text-center p-4 mt-3">
+              <h5 class="fw-bold mb-0"><span class="text-info">Carie</span> Lanster</h5>
+              <small>CEO of Food and Beverage</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+          <div class="rounded shadow overflow-hidden">
+            <div class="position-relative">
+              <img class="img-fluid" src="https://placehold.co/500" alt="" />
+              <div
+                class="position-absolute start-50 top-100 translate-middle d-flex align-items-center"
+              >
+                <a class="btn btn-square btn-secondary mx-1" href=""
+                  ><i class="bi bi-facebook"></i
+                ></a>
+                <a class="btn btn-square btn-secondary mx-1" href=""
+                  ><i class="bi bi-twitter"></i
+                ></a>
+                <a class="btn btn-square btn-secondary mx-1" href=""
+                  ><i class="bi bi-instagram"></i
+                ></a>
+              </div>
+            </div>
+            <div class="text-center p-4 mt-3">
+              <h5 class="fw-bold mb-0"><span class="text-info">Andy</span> Gostly</h5>
+              <small>CEO of Human Resources</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
+          <div class="rounded shadow overflow-hidden">
+            <div class="position-relative">
+              <img class="img-fluid" src="https://placehold.co/500" alt="" />
+              <div
+                class="position-absolute start-50 top-100 translate-middle d-flex align-items-center"
+              >
+                <a class="btn btn-square btn-secondary mx-1" href=""
+                  ><i class="bi bi-facebook"></i
+                ></a>
+                <a class="btn btn-square btn-secondary mx-1" href=""
+                  ><i class="bi bi-twitter"></i
+                ></a>
+                <a class="btn btn-square btn-secondary mx-1" href=""
+                  ><i class="bi bi-instagram"></i
+                ></a>
+              </div>
+            </div>
+            <div class="text-center p-4 mt-3">
+              <h5 class="fw-bold mb-0"><span class="text-info">Benji</span> Mui</h5>
+              <small>Chief Engineer</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
